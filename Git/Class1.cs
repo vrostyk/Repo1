a3f45e18ae7612d5e132302b4254442a51fa6c41
vrostyk <vrostyk12@gmail.com>
2018-01-08 16:03:03 +0200

    F1

---
+++ Git/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git
+{
+    public class Class1
+    {
+        int amount = 10;
+        public int Method(int amount)
+        {
+            return amount;
+        }
+    }
+}
---
+++ Git_V/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Git;
+
+namespace Git_V
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Console.WriteLine(class1.Method(10));
+        }
+    }
+}
